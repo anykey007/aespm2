d56f2eda63fef98cb0350ac405ccc7b4b3a1db7a
anykey007 <anykeyua@gmail.com>
2012-09-17 11:15:45 +0300

    add some changes to design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+
+  layout :layout
+  before_filter :authenticate_user! 
+  
   protect_from_forgery
-  def authenticate_admin
+  def authenticate_admin  	 
     if !current_user.admin?
       redirect_to root_url
     end
+  end
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    is_a?(Devise::SessionsController) ? "login" : "application"
+    # or turn layout off for every devise controller:
+    # devise_controller? && "application"
   end
 
 end
