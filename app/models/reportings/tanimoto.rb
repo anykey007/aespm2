b30149fb75bd05014832ab9badf2b4d10f83fbc7
anykey007 <anykeyua@gmail.com>
2013-03-31 22:02:00 +0300

    shange logic for tanimoto calc

--- app/models/reportings/tanimoto.rb
+++ app/models/reportings/tanimoto.rb
@@ -3,14 +3,20 @@ class Reportings::Tanimoto < Report
      ReportingForms::Tanimoto.all
   end
 
+  def count_one
+    arr = values.map(&:value1)
+    arr.delete(0)
+    arr.size
+  end
+
   def compare_tanimoto(other_report)
   	general_count = 0
   	values.each_index do |i|
-  		if(values[i].value1==other_report.values[i].value1)
+  		if(values[i].value1==other_report.values[i].value1 && values[i].value1==1)
   			general_count+=1
   		end
   	end
-  	k = ((general_count+0.0)/(values.length*2-general_count)).round(2)
+  	k = ((general_count+0.0)/(count_one+other_report.count_one-general_count)).round(2)
   end
 
   def compare_tanimotos(report_ids)
@@ -19,11 +25,11 @@ class Reportings::Tanimoto < Report
       report = Reportings::Tanimoto.find(id)
       general_count = 0
       values.each_index do |i|
-        if(values[i].value1==report.values[i].value1)
+        if(values[i].value1==report.values[i].value1 && values[i].value1==1)
           general_count+=1
         end
       end
-      coef = ((general_count+0.0)/(values.length*2-general_count)).round(2)
+      coef = ((general_count+0.0)/(count_one+report.count_one-general_count)).round(2)
       coefs << coef
     end
     Hash[*[report_ids,coefs].transpose.flatten]
